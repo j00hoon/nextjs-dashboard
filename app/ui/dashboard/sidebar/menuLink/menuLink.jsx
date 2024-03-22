6e86ef1aef57c023887f93f3dff69adbc4d9e1e8
Seunghoon Baik <j00hoon1101@gmail.com>
2024-03-22 13:20:24 -0400

    Navbar & sidebar & css are updated on dashboard page

--- app/ui/dashboard/sidebar/menuLink/menuLink.jsx
+++ app/ui/dashboard/sidebar/menuLink/menuLink.jsx
@@ -11,11 +11,11 @@ const MenuLink = ({item}) => {
     const pathname = usePathname();
 
 
-
     return (
         <Link
             href={item.path} 
-            className={`${styles.container} && ${pathname === item.path && styles.active}`}
+            className={`${styles.container} 
+                && ${pathname === item.path && styles.active}`}
         >
             {item.icon}
             {item.title}
--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -119,6 +119,12 @@ const Sidebar = () => {
           ))}
 
         </ul>
+
+        <button className={styles.logout}>
+          <MdLogout />
+          Logout
+        </button>
+        
       </div>
     )
   }
