557cdda67cb1d7b10d0e87fb3dc71b1020055fd5
Seunghoon Baik <j00hoon1101@gmail.com>
2024-03-25 11:31:24 -0400

    Right bar on dashboard page is added

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./rightbar.module.css";
+import { MdPlayCircleFilled } from "react-icons/md";
 
 
 
@@ -9,22 +10,68 @@ import styles from "./rightbar.module.css";
 const Rightbar = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.item}>
-                
+
+
+
+
+            <div className={styles.item}> 
                 <div className={styles.bgContainer}>
                     <Image 
                         src="/astronaut.png"
                         alt=""
                         fill
+                        className={styles.bg}
                     />
                 </div>
-                <div className={styles.texts}>
+                <div className={styles.text}>
                     <span className={styles.notification}>
                         🔥 Available Now
                     </span>
+                    <h3 className={styles.title}>
+                        How to use the new version of the admin dashboard?
+                    </h3>
+                    <span className={styles.subtitle}>
+                        Takes 4 minutes to learn
+                    </span>
+                    <p className={styles.desc}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Reprehenderit eius libero perspiciatis recusandae possimus.
+                    </p>
+                    <button className={styles.button}>
+                        <MdPlayCircleFilled />
+                        Watch
+                    </button>
                 </div>
-
             </div>
+
+
+
+
+            <div className={styles.item}>
+                <div className={styles.text}>
+                    <span className={styles.notification}>
+                        🔥 Available Now
+                    </span>
+                    <h3 className={styles.title}>
+                        How to use the new version of the admin dashboard?
+                    </h3>
+                    <span className={styles.subtitle}>
+                        Takes 4 minutes to learn
+                    </span>
+                    <p className={styles.desc}>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Reprehenderit eius libero perspiciatis recusandae possimus.
+                    </p>
+                    <button className={styles.button}>
+                        <MdPlayCircleFilled />
+                        Watch
+                    </button>
+                </div>
+            </div>
+
+
+
+
         </div>
     );
 }
