b4317b6606ba8e56d154f499a7f906a41fa9f83b
Seunghoon Baik <j00hoon1101@gmail.com>
2024-03-25 14:37:18 -0400

    User page is added

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,7 +1,74 @@
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import Search from "@/app/ui/dashboard/search/search";
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
+
+
+
+
 const Users = () => {
     return (
-      <div>Users</div>
-    )
+      <div className={styles.container}>
+        
+        <div className={styles.top}>
+          <Search placeholder="Search for a user" />
+          <Link href="/dashboard/users/add">
+            <button className={styles.addButton}>
+              Add new
+            </button>
+          </Link>
+        </div>
+        
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <td>Name</td>
+              <td>Email</td>
+              <td>Created at</td>
+              <td>Role</td>
+              <td>Status</td>
+              <td>Action</td>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>
+                <div className={styles.user}>
+                  <Image 
+                    src="/noavatar.png" 
+                    alt="" 
+                    width={40} 
+                    height={40} 
+                    className={styles.userImage} 
+                  />
+                  Seunghoon Baik
+                </div>
+              </td>
+              <td>test@gmail.com</td>
+              <td>03.25.2024</td>
+              <td>Admin</td>
+              <td>active</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href="/">
+                    <button className={`${styles.button} ${styles.view}`}>View</button>
+                  </Link>
+                  <Link href="/">
+                    <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+                  </Link>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <Pagination />
+
+      </div>
+    );
   }
   
   export default Users;
---
+++ app/ui/dashboard/footer/footer.jsx
@@ -0,0 +1,15 @@
+import styles from "./footer.module.css";
+
+
+const Footer = () => {
+    return (
+        <div className={styles.container}>
+            Footer
+            <div className={styles.text}>
+                © All rights reserved.
+            </div>
+        </div>
+    );
+}
+  
+  export default Footer;
