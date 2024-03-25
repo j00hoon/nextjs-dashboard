bac0b976c35d310a50d885880fa7cd18de1215bb
Seunghoon Baik <j00hoon1101@gmail.com>
2024-03-25 19:58:16 -0400

    Product page is added

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,7 +1,75 @@
+import Image from "next/image";
+import Link from "next/link";
+import Search from "@/app/ui/dashboard/search/search";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+
+import styles from "@/app/ui/dashboard/products/products.module.css";
+
+
+
+
+
 const Products = () => {
-    return (
-      <div>Products</div>
-    )
-  }
+  return (
+    <div className={styles.container}>
+        
+        <div className={styles.top}>
+          <Search placeholder="Search for a product" />
+          <Link href="/dashboard/products/add">
+            <button className={styles.addButton}>
+              Add new
+            </button>
+          </Link>
+        </div>
+        
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <td>Title</td>
+              <td>Description</td>
+              <td>Price</td>
+              <td>Created at</td>
+              <td>Stock</td>
+              <td>Action</td>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>
+                <div className={styles.product}>
+                  <Image 
+                    src="/noproduct.jpg" 
+                    alt="" 
+                    width={40} 
+                    height={40} 
+                    className={styles.productImage} 
+                  />
+                  IPhone
+                </div>
+              </td>
+              <td>Description</td>
+              <td>$999</td>
+              <td>03.25.2024</td>
+              <td>active</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href="/">
+                    <button className={`${styles.button} ${styles.view}`}>View</button>
+                  </Link>
+                  <Link href="/">
+                    <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+                  </Link>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <Pagination />
+
+      </div>
+  );
+}
   
   export default Products;
