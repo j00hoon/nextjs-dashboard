da0d21e366cdb52a6bed1031759676b09be7cdb0
Seunghoon Baik <j00hoon1101@gmail.com>
2024-03-26 21:53:08 -0400

    Add function for User & Product is added

---
+++ app/dashboard/users/add/page.jsx
@@ -0,0 +1,36 @@
+import styles from "@/app/ui/dashboard/users/add/add.module.css";
+
+
+
+
+const AddUser = () => {
+    return (
+        <div className={styles.container}>
+            <form action="" className={styles.form}>
+                <input type="text" placeholder="username" name="username" required />
+                <input type="email" placeholder="email" name="email" required />
+                <input type="password" placeholder="password" name="password" required />
+                <input type="phone" placeholder="phone" name="phone" required />
+                <select name="isAdmin" id="isAdmin">
+                    <option value={false} selected>Is Admin?</option>
+                    <option value={true}>Yes</option>
+                    <option value={false}>No</option>
+                </select>
+                <select name="isActive" id="isActive">
+                    <option value={false} selected>Is Active?</option>
+                    <option value={true}>Yes</option>
+                    <option value={false}>No</option>
+                </select>
+                <textarea 
+                    name="address"
+                    id="address"
+                    rows="15"
+                    placeholder="Address"
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default AddUser;
