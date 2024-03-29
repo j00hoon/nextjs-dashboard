6e4f8d5b884108738101672fe01affa60d9ceb22
Seunghoon Baik <j00hoon1101@gmail.com>
2024-03-28 21:54:57 -0400

    Users list from mongo db on users page

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -15,7 +15,7 @@ const Users = async () => {
 
   const users = await fetchUsers();
 
-  console.log("users : " + users);
+  
 
 
 
@@ -46,34 +46,38 @@ const Users = async () => {
         </thead>
 
         <tbody>
-          <tr>
-            <td>
-              <div className={styles.user}>
-                <Image 
-                  src="/noavatar.png" 
-                  alt="" 
-                  width={40} 
-                  height={40} 
-                  className={styles.userImage} 
-                />
-                Seunghoon Baik
-              </div>
-            </td>
-            <td>test@gmail.com</td>
-            <td>03.25.2024</td>
-            <td>Admin</td>
-            <td>active</td>
-            <td>
-              <div className={styles.buttons}>
-                <Link href="/dashboard/users/test">
-                  <button className={`${styles.button} ${styles.view}`}>View</button>
-                </Link>
-                <Link href="/">
-                  <button className={`${styles.button} ${styles.delete}`}>Delete</button>
-                </Link>
-              </div>
-            </td>
-          </tr>
+          {users.map((user) => (
+
+            <tr key={user.id}>
+              <td>
+                <div className={styles.user}>
+                  <Image 
+                    src={user.img || "noavatar.png"}
+                    alt="" 
+                    width={40} 
+                    height={40} 
+                    className={styles.userImage}
+                  />
+                  {user.username}
+                </div>
+              </td>
+              <td>{user.email}</td>
+              <td>{user.createdAt.toString().slice(4,16)}</td>
+              <td>{user.isAdmin ? "Admin" : "Client"}</td>
+              <td>{user.isActive ? "Active" : "Passive"}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/users/${user.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>View</button>
+                  </Link>
+                  <Link href="/dashboard/users">
+                    <button className={`${styles.button} ${styles.delete}`}>Delete</button>
+                  </Link>
+                </div>
+              </td>
+            </tr>
+
+          ))}
         </tbody>
       </table>
 
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns : [
+            {
+                protocol:"https",
+                hostname:"media.ed.edmunds-media.com"
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
